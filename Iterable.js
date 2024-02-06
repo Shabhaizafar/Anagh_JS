57fee6fb44f3652423f2f4415de4e9f71a5cbf30
Shabhai Zafar <shabhaizafar@gmail.com>
2024-02-06 16:44:19 +0530

    Map

---
+++ Iterable.js
@@ -0,0 +1,30 @@
+//What is a Iterable in JS :(for of loop)
+
+// Iterable:  string ,Array.
+
+// Not iterable : Objects, Functions.
+
+// var str = "Royal";
+// for (const iterator of "true") {
+//     console.log(iterator);
+// }
+
+
+// var Arr = [1,2,3,4];
+// // for (const iterator of [1,2,3,4,,5,6,7,8]) {
+// //     console.log(iterator);
+// // }
+
+
+// var myobj = {
+//     fname : "Raj",
+//     lname : "Patel",
+//     Age : 1,
+//     Gender : "Male"
+// }
+// for (const iterator of myobj) {
+//     console.log(iterator);
+// }
+// console.log( Array.isArray(Arr));
+// console.log( Array.isArray(myobj));
+// console.log( Object.isExtensible(Arr));
---
+++ Map.js
@@ -0,0 +1,63 @@
+// Map :
+// it's iterable.
+// order based
+// var myMap = new Map();
+
+// // myMap.fname = "zafar";
+// // myMap.lname = "z";
+
+// myMap.set("fname","Raj");
+// myMap.set("fname","R");
+// myMap.set(2, "Kumar");
+// console.log(myMap);
+
+
+
+// var obj = {
+//     fname : "Zafar",
+//     2 : "z",
+//     fname : "Z"
+// }
+// console.log(obj);
+
+// for (const key in obj) {
+//     console.log(key,typeof key);
+// }
+// for (const key of myMap) {  
+//     console.log(key[0],typeof key[0]);
+// }
+
+// console.log(myMap.get("fname"));
+
+// myMap.clear();
+// console.log(myMap);
+
+// console.log(myMap.size);
+// console.log(myMap.entries());
+// console.log(myMap.keys());
+// console.log(myMap.values());
+// myMap.delete(2)
+// console.log(myMap);
+
+// console.log(myMap.has("kumar"));
+
+
+/* 
+
+01)  Create an empty Map and add key-value pairs to it.
+02)  Initialize a Map with key-value pairs and iterate over them using forEach.
+03)  Check if a specific key exists in a Map.
+04)  Get the size of a Map.
+05)  Retrieve the value associated with a specific key in a Map.
+06)  Remove a key-value pair from a Map.
+07)  Clear all key-value pairs from a Map.
+08)  Convert a Map to an array of key-value pairs.
+09)  Convert an array of key-value pairs to a Map.
+10) Merge two Maps together.
+11) Check if two Maps are equal.
+12) Use Map's has(), get(), set(), and delete() methods.
+13) Use Map's keys(), values(), and entries() methods.
+14) Use spread operator to create a new Map from an existing Map.
+15) Use Map to count occurrences of items in an array.
+
+*/
